9805c2c6035ba835fe96fec0c0fe5b1e93fa014e
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2018-11-15 14:26:24 -0800

    here you go

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+
+Global variables (variables that are declared outside of a function):
+word - string (according to Gabe when it is selecting every time?)
+words - array
+guesses - number
+guessedLetters - array
+*/
+var words = ["apple", "pear", "orange", "lemon"];
+var guesses = 0;
+var guessedLetters = [];
+
+// Reset the board, clear out any traces of the last round from guessedLetters
+// Choose a word - this code will grab  a random element from your words array for you:
+// var rand = myArray[Math.floor(Math.random() * myArray.length)];
+function startGame(){
+
+}
+
+// Compare word to guessedLetters using guessedLetters.indexOf(letter in word) to
+// build the “_” word with the correctly guessed letters filled in.
+function printWord(){
+
+}
+
+// Take the most recently guessed letter and validate it, re-print the word, deduct from guesses,
+// check to see if the user has won or lost yet.
+function guessLetter(){
+
+}
