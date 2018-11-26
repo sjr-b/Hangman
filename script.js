d84651712f383232ee8df157aa8f452977ab93a3
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2018-11-26 14:41:33 -0800

    here you go

--- script.js
+++ script.js
@@ -14,17 +14,25 @@ var guessedLetters = [];
 // Choose a word - this code will grab  a random element from your words array for you:
 // var rand = myArray[Math.floor(Math.random() * myArray.length)];
 function startGame(){
-
-}
-
-// Compare word to guessedLetters using guessedLetters.indexOf(letter in word) to
-// build the “_” word with the correctly guessed letters filled in.
-function printWord(){
-
+    var chosenWord = words[Math.floor(Math.random() * words.length)];
+    var chosenWordLength = chosenWord.length;
+    document.getElementById("guessNumber").innerHTML = chosenWordLength - 1;
+    for (var a = 0; a < chosenWordLength; a++){
+        document.getElementById("word").innerHTML += "_ ";
+    }
 }
 
 // Take the most recently guessed letter and validate it, re-print the word, deduct from guesses,
 // check to see if the user has won or lost yet.
 function guessLetter(){
+    var letter = document.getElementById("letter").value;
+    if (letter = ""){
+
+    }
+}
+
+// Compare word to guessedLetters using guessedLetters.indexOf(letter in word) to
+// build the “_” word with the correctly guessed letters filled in.
+function printWord(){
 
 }
