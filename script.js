2dc54c068d329e75de260e38c5d8c4aee99f3ae6
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2018-12-04 14:26:20 -0800

    fill categories

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ words - array
 guesses - number
 guessedLetters - array
 */
-var words = ["a", "ab"];
-var word = words[Math.floor(Math.random() * words.length)]
-var guesses = 0;
+var defaultWord = ["ab", "abc", "abcd"];
+var bhs = ["albinson", "henri", "schweng", "zapata", "aperribay", "quiroz", "heffner", "wray", "maaze", "tobias", "becker", "bissell", "barcelos", "halpern"];
+var langEasy= [];
+var langHard = [];
+var word = defaultWord[Math.floor(Math.random() * defaultWord.length)];
+var guesses = word.length - 1;
 var guessedLetters = [];
 
 // Reset the board, clear out any traces of the last round from guessedLetters
 // Choose a word - this code will grab  a random element from your words array for you:
 // var rand = myArray[Math.floor(Math.random() * myArray.length)];
 function startGame(){
-    var chosenWordLength = word.length;
-    document.getElementById("guessNumber").innerHTML = chosenWordLength - 1;
+    document.getElementById("guessNumber").innerHTML = guesses;
     document.getElementById("word").innerHTML = printWord();
 }
 
@@ -27,7 +29,16 @@ function guessLetter(){
     document.getElementById("letter").options[matchValueToPosition(letter)].disabled = true;
     guessedLetters.push(letter);
     document.getElementById("guessedLetters").innerHTML = guessedLetters;
-    document.getElementById("word").innerHTML = printWord();
+    var result = printWord()
+    document.getElementById("word").innerHTML = result;
+    if (result == word){
+        alert("Congratulations");
+    }
+    document.getElementById("guessNumber").innerHTML = guesses;
+    if (guesses == 0){
+        alert("You have lost!");
+        document.getElementById("letter").disabled = true;
+    }
 }
 
 // Compare word to guessedLetters using guessedLetters.indexOf(letter in word) to
@@ -39,11 +50,16 @@ function printWord(){
             for (var c = 0; c < guessedLetters.length; c++){
                 if (guessedLetters[c] == word[a]){
                     wordProgress += guessedLetters[c];
-                } else {
-                    wordProgress += "_";
+                }
+                if (wordProgress == word){
+                    return wordProgress;
                 }
             }
+            if (wordProgress.length - 1 != a){
+                wordProgress += "_";
+            }
         }
+        guesses--;
     } else {
         for (var b = 0; b < word.length; b++){
             wordProgress += "_";
@@ -52,6 +68,7 @@ function printWord(){
     return wordProgress;
 }
 
+// This function matches a letter to its position in the alphabet so that the letter selection box can be disabled when a letter has been guessed.
 function matchValueToPosition(letter){
     var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     for (var b = 0; b < 26; b++){
@@ -59,4 +76,10 @@ function matchValueToPosition(letter){
             return b;
         }
     }
+}
+
+// This function applies the student's choice to change what the word is (so it's not the default).
+function applySelection(){
+    var category = document.getElementById("categories").value;
+    word = category[Math.floor(Math.random() * category.length)];
 }
