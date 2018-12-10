2607b1ab097f0727e441d346171d076dc9267ea3
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2018-12-10 14:41:18 -0800

    check to see if the victory gif works mwahahahaha

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var hangmanImages = ["https://upload.wikimedia.org/wikipedia/commons/7/70/Hangman-2.png", "https://upload.wikimedia.org/wikipedia/commons/9/97/Hangman-3.png", "https://upload.wikimedia.org/wikipedia/commons/2/27/Hangman-4.png", "https://upload.wikimedia.org/wikipedia/commons/6/6b/Hangman-5.png", "https://upload.wikimedia.org/wikipedia/commons/d/d6/Hangman-6.png"];
-var defaultWord = ["pear", "apple"/*, "strawberry", "durian", "orange", "starfruit"*/];
+var hangmanImages = ["https://upload.wikimedia.org/wikipedia/commons/8/8b/Hangman-0.png", "https://upload.wikimedia.org/wikipedia/commons/7/70/Hangman-2.png", "https://upload.wikimedia.org/wikipedia/commons/9/97/Hangman-3.png", "https://upload.wikimedia.org/wikipedia/commons/2/27/Hangman-4.png", "https://upload.wikimedia.org/wikipedia/commons/6/6b/Hangman-5.png", "https://upload.wikimedia.org/wikipedia/commons/d/d6/Hangman-6.png"];
+var defaultWord = ["pear", "apple", "strawberry", "durian", "orange", "starfruit"];
 var bhs = ["albinson", "henri", "schweng", "zapata", "aperribay", "quiroz", "heffner", "wray", "maaze", "tobias", "becker", "bissell", "barcelos", "halpern", "austera",];
 var langEasy= ["english", "spanish", "hindi", "mandarin", "arabic", "swahili", "cherokee"];
 var langHard = ["aymara", "guguyimidjir", "wolof", "azerbaijani", "basque", "occitan", "inupiaq", "ainu", "liki"];
@@ -11,53 +11,67 @@ var guessedLetters = [];
 function startGame(){
     document.getElementById("guessNumber").innerHTML = guesses;
     document.getElementById("word").innerHTML = printWord();
+    document.getElementById("endGame").style.display = "none";
+    document.getElementById("image").innerHTML = "";
 }
 
-// Take the most recently guessed letter and validate it, re-print the word, deduct from guesses,
-// check to see if the user has won or lost yet.
+// This will take the user's guessed letter and use it accordingly.
 function guessLetter(){
     var letter = document.getElementById("letter").value;
+    // This deactivates the selected letter's button.
     document.getElementById("letter").options[matchValueToPosition(letter)].disabled = true;
+    // This updates the list of already guessed letters.
     guessedLetters.push(letter);
     document.getElementById("guessedLetters").innerHTML = guessedLetters;
+    // This updates the word and, if applicable, informs the user of their victory/tragic loss.
     var result = printWord();
     document.getElementById("word").innerHTML = result;
     if (result == word){
-        alert("Congratulations, you have won! The game is now going to reset.");
-        applySelection();
+        alert("Congratulations, you have won! You may now choose to either bask in your victory by looking at this page, or restart the game and try again.");
+        document.getElementById("endGame").style.display = "inline";
+        document.getElementById("img").innerHTML = "";
+        var victory = document.createElement("img");
+        victory.setAttribute("src", "http://replygif.net/i/288.gif");
+        victory.setAttribute("width", "200px");
+        document.getElementById("img").appendChild(victory);
     }
     document.getElementById("guessNumber").innerHTML = guesses;
     if (guesses == 0){
-        alert("You have lost. The word was " + word + ". The game will now restart.");
-        applySelection();
+        alert("You have lost. The word was " + word + ". You can play another round if you want. Better luck next time!");
+        document.getElementById("endGame").style.display = "inline";
     }
+    // This inputs the appropriate picture.
+    document.getElementById("image").innerHTML = "";
     var picture = document.createElement("img");
     picture.setAttribute("src", giveImage());
+    picture.setAttribute("width", "200px");
     document.getElementById("image").appendChild(picture);
 }
 
-// Compare word to guessedLetters using guessedLetters.indexOf(letter in word) to
-// build the “_” word with the correctly guessed letters filled in.
+// This function creates the updated word in progress.
 function printWord(){
     var wordProgress = "";
-    if (guessedLetters.length != 0){
-        for (var a = 0; a < word.length; a++){
-            for (var c = 0; c < guessedLetters.length; c++){
+    if (guessedLetters.length != 0){ // this line checks to see if the user has inputted something or if the game has just begun
+        for (var a = 0; a < word.length; a++){ // this part checks each letter of the word
+            for (var c = 0; c < guessedLetters.length; c++){ // this checks each guessed letter
                 if (guessedLetters[c] == word[a]){
                     wordProgress += guessedLetters[c];
                 }
+                // This is for when the word has been completely filled and needs to be inputted out.
                 if (wordProgress == word){
                     return wordProgress;
                 }
             }
+            // This is for if none of the guessed letters matched this particular spot in the word.
             if (wordProgress.length - 1 != a){
                 wordProgress += "_";
             }
         }
+        // If the guessed letter was not in the word, this updates the number of guesses the user has left.
         if (document.getElementById("word").innerHTML == wordProgress){
             guesses--;
         }
-    } else {
+    } else { // this is the default
         for (var b = 0; b < word.length; b++){
             wordProgress += "_";
         }
@@ -75,7 +89,7 @@ function matchValueToPosition(letter){
     }
 }
 
-// This function applies the student's choice to change what the word is (so it's not the default).
+// This function applies the student's choice to change what the word is.
 function applySelection(){
     var category = document.getElementById("categories").value;
     if (category == "bhs"){
@@ -119,10 +133,19 @@ function guessNumber(){
 
 // This function puts images into the guess box to indicate the number of guesses left.
 function giveImage(){
-    var defaultGuess = guessNumber();
-    if (guesses != defaultGuess){ // this way there is only an image if there have been any wrong guesses.
-        if (guesses == 5){
-            return hangmanImages[0];
-        }
+    if (guesses == 6){
+        return hangmanImages[6];
+    } else if (guesses == 5){
+        return hangmanImages[0];
+    } else if (guesses == 4){
+        return hangmanImages[1];
+    } else if (guesses == 3){
+        return hangmanImages[2];
+    } else if (guesses == 2){
+        return hangmanImages[3];
+    } else if (guesses == 1){
+        return hangmanImages[4];
+    } else {
+        return hangmanImages[5];
     }
 }
